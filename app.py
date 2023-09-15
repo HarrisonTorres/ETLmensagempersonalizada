10a3ff53db6307299a87db00938dbc0c5481763a
harrison torres <harri.torres12@gmail.com>
2023-09-15 00:49:19 -0300

    Firsh Commit

---
+++ app.py
@@ -0,0 +1,42 @@
+# Utilize sua própria URL se quiser ;)
+# Repositório da API: https://github.com/digitalinnovationone/santander-dev-week-2023-api
+sdw2023_api_url = 'https://sdw-2023-prd.up.railway.app'
+
+import requests
+import json
+import pandas as pd
+
+df = pd.read_csv('SDW2023.csv')
+user_ids = df['UserID'].tolist()
+print(user_ids)
+
+def get_user(id):
+  response = requests.get(f'{sdw2023_api_url}/users/{id}')
+  return response.json() if response.status_code == 200 else None
+
+users = [user for id in user_ids if (user := get_user(id)) is not None]
+print(json.dumps(users, indent=2))
+
+
+def generate_ai_news(user):
+  completion = []
+    model="gpt-4",
+    messages=[
+      {
+          "role": "system",
+          "content": "Você é um especialista em markting bancário."
+      },
+      {
+          "role": "user",
+          "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
+      }
+    ]
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+  user['news'].append({
+      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news
+  })
